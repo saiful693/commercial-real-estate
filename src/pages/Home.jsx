fd8ab01b7967c9ea90b11a4a6dd1f701c01ee827
saiful693 <bdsaiful770@gamil.com>
2024-06-05 14:41:45 +0900

    Footer section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <div className="mt-16">
-                <h1 className="text-4xl font-bold text-green-400 uppercase">Commercial</h1>
+                <h1 className="text-4xl font-bold text-green-400 uppercase mt-20 mb-10 text-center">Commercial</h1>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
                     propertys.map(property=> <Estate key={property.id} property={property}></Estate>)
